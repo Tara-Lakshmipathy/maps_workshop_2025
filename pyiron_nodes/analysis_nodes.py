14191255abfc11a7396a8a48f04f71ae7dd720ad
Tarakeshwar Lakshmipathy <87063936+Tara-Lakshmipathy@users.noreply.github.com>
2025-01-15 15:25:39 +0100

    Update analysis_nodes.py

--- pyiron_nodes/analysis_nodes.py
+++ pyiron_nodes/analysis_nodes.py
@@ -19,7 +19,7 @@ def PlotTemperatureSteps(job):
     temperatures = job.output.temperature
     steps = job.output.steps
     plt.plot(steps, temperatures)
-    plt.xlabel('MD step')
+    plt.xlabel('Steps')
     plt.ylabel('Temperature [K]');
     
     return plt.show()
@@ -33,7 +33,7 @@ def PlotVolumeSteps(job):
     volumes = job.output.volume
     steps = job.output.steps
     plt.plot(steps, volumes)
-    plt.xlabel('MD step')
+    plt.xlabel('Steps')
     plt.ylabel('Volume [A^3]');
     
     return plt.show()
@@ -47,7 +47,7 @@ def PlotPotentialEnergySteps(job):
     energy_pot = job.output.energy_pot
     steps = job.output.steps
     plt.plot(steps, energy_pot)
-    plt.xlabel('MD step')
+    plt.xlabel('Steps')
     plt.ylabel('Potential energy [eV]');
     
     return plt.show()
@@ -61,7 +61,7 @@ def PlotTotalEnergySteps(job):
     energy_tot = job.output.energy_tot
     steps = job.output.steps
     plt.plot(steps, energy_tot)
-    plt.xlabel('MD step')
+    plt.xlabel('Steps')
     plt.ylabel('Total energy [eV]');
     
     return plt.show()
@@ -79,14 +79,38 @@ def PlotPressureSteps(job):
     plt.plot(steps, press_xx, label="pressure_xx")
     plt.plot(steps, press_yy, label="pressure_yy")
     plt.plot(steps, press_zz, label="pressure_zz")
-    plt.xlabel('MD step')
+    plt.xlabel('Steps')
     plt.ylabel('Pressure [bar]');
     plt.legend(loc='best')
     
     return plt.show()
 
+@as_function_node("final_total_energy")
+def ExtractFinalTotalEnergy(job):
+    """
+    job: pyiron_atomistics job
+    """
+       
+    return job.output.energy_tot[-1]
+
+@as_function_node("final_potential_energy")
+def ExtractFinalPotentialEnergy(job):
+    """
+    job: pyiron_atomistics job
+    """
+       
+    return job.output.energy_pot[-1]
+
+@as_function_node("final_cell_volume")
+def ExtractFinalCellVolume(job):
+    """
+    job: pyiron_atomistics job
+    """
+       
+    return job.output.volume[-1]
+
 @as_function_node("results")
-def MurnaghanResults(murn):
+def MurnaghanResultsSummary(murn):
     """
     murn: murnagha job
     """
@@ -96,3 +120,35 @@ def MurnaghanResults(murn):
     display(murn.plot())
     
     return murn.output_to_pandas()
+
+@as_function_node("equil_energy")
+def ExtractEquilibriumEnergy(murn):
+    """
+    murn: murnagha job
+    """
+    
+    return murn.output_to_pandas()["equilibrium_energy"][0]
+
+@as_function_node("equil_volume")
+def ExtractEquilibriumVolume(murn):
+    """
+    murn: murnagha job
+    """
+    
+    return murn.output_to_pandas()["equilibrium_volume"][0]
+
+@as_function_node("bulk_modulus")
+def ExtractBulkModulus(murn):
+    """
+    murn: murnagha job
+    """
+    
+    return murn.output_to_pandas()["equilibrium_bulk_modulus"][0]
+
+@as_function_node("b_prime")
+def ExtractBPrime(murn):
+    """
+    murn: murnagha job
+    """
+    
+    return murn.output_to_pandas()["equilibrium_b_prime"][0]
